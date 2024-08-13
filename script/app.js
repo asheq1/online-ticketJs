2bf44c44c0658ebff231c7d143764b95a82db8ec
asheq1 <bsmrstu18fmb025@gmail.com>
2024-08-13 22:39:20 +0600

    toggle  navbar

---
+++ script/app.js
@@ -0,0 +1,12 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden');
+
+    // Toggle between hamburger and cross icon
+    this.classList.toggle('open');
+    if (this.classList.contains('open')) {
+        this.innerHTML = '<span class="block w-8 h-1 bg-white mb-1 transform rotate-45 translate-y-2"></span><span class="block w-8 h-1 bg-white mb-1 opacity-0"></span><span class="block w-8 h-1 bg-white transform -rotate-45 -translate-y-2"></span>';
+    } else {
+        this.innerHTML = '<span class="block w-8 h-1 bg-white mb-1"></span><span class="block w-8 h-1 bg-white mb-1"></span><span class="block w-8 h-1 bg-white"></span>';
+    }
+});
