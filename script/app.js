761962e943531d86e4749a6e4d8ad496eaab9f10
asheq1 <bsmrstu18fmb025@gmail.com>
2024-08-15 22:24:31 +0600

    coupon

--- script/app.js
+++ script/app.js
@@ -33,6 +33,63 @@ seats.forEach((seat) => {
         let seatCount = getElementId('seat-count');
         let perSeat =  seatCount + 1;
         setElementId('seat-count', perSeat);
+        if(seatCount > 3){
+            // alert('No more ticket')
+            return 
+        }
+
+        // left seat
+        let leftSeat = getElementId('seat-left');
+        let leftCount = leftSeat - 1;
+        setElementId('seat-left', leftCount);
+        
+        let ticketPrice = 550;
+        // seat, class, price
+        const container = document.getElementById('selected-container');
+        const newRow = document.createElement('tr');
+        newRow.innerHTML = `
+                <td>${seatLevel}</td>
+                <td>Economy</td>
+                <td>${ticketPrice}</td>
+        `;
+        container.appendChild(newRow);
+
+        // total ticket price 
+        let totalPrice = perSeat * ticketPrice;
+        setElementId('total-ticket-price', totalPrice);
+
+        // Get the input field and apply button elements
+        const couponCode = document.getElementById('coupon-code');
+        const applyButton = document.getElementById('apply-now');
+
+        // coupon code 
+        couponCode.addEventListener('keyup', ()=>{
+            const inputValue = couponCode.value;
+            if(inputValue === 'NEW15'){
+                applyButton.removeAttribute('disabled')
+            } 
+            // else {
+            //     applyButton.removeAttribute('disabled', true)
+            // }
+        })
+        // coupon discuont 
+        applyButton.addEventListener('click', () => {
+            const inputValue = couponCode.value;
+
+            if(inputValue === 'NEW15'){
+                const discount15 = totalPrice * 0.15;
+                setElementId('discount-price', discount15)
+                couponCode.value =''
+            }
+        })
+
+        // grand total
+        const grand_Field = getElementId('grand-total');
+        const grand_Price = getElementId('total-ticket-price');
+        const discount = getElementId('discount-price');
+        const grand_total = grand_Price + grand_Field;
+        const all_total = grand_total + discount;
+        setElementId('grand-total', all_total)
 
     })
 })
